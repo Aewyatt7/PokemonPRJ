c4bdc2eb9d1fdd3d61a139a9bf51aa4e4d6d5642
Andrew Wyatt <aewyatt7@gmail.com>
2024-02-05 12:05:47 -0700

    pokePRJ

--- src/PokemonImg.jsx
+++ src/PokemonImg.jsx
@@ -1,29 +1,57 @@
 import React from "react";
 
-const PokemonImg = ({ pokemon }) => {
+function PokemonImg({ pokemon }) {
+  const convertMetricsToImperial = (heightInMeters, weightInKg) => {
+    const heightInFeet = Math.floor(heightInMeters * 3.28084);
+    const remainingInches = Math.round((heightInMeters * 39.3701) % 12);
+    const weightInLbs = Math.round(weightInKg * 2.20462);
+
+    return {
+      height: { feet: heightInFeet, inches: remainingInches },
+      weight: weightInLbs,
+    };
+  };
+
+  const { height, weight } = convertMetricsToImperial(
+    parseFloat(pokemon.height.replace(" m", "")),
+    parseFloat(pokemon.weight.replace(" kg", ""))
+  );
+
   const typeImages = pokemon.type.map((type) => `${type}.png`);
-  const weaknessImages = pokemon.weaknesses.map((weakness) => `${weakness}.png`);
+  const weaknessImages = pokemon.weaknesses.map(
+    (weakness) => `${weakness}.png`
+  );
+
+  const typeClass = pokemon.type[0].toLowerCase(); 
 
   return (
-    <div className="col">
+    <div className={`col ${typeClass}`}>
       <p>{pokemon.name}</p>
       <img src={pokemon.img} alt={`Image of ${pokemon.name}`} />
-      <p>Height: {pokemon.height}</p>
-      <p>Weight: {pokemon.weight}</p>
-      {pokemon.next_evolution && pokemon.next_evolution.length > 0 && (
-    <p>Next Evolution: {pokemon.next_evolution[0].name}</p>
-)}
-       <p> Types:
+      <p>
+        Height: {height.feet}' {height.inches}"
+      </p>
+      <p>Weight: {weight} lbs</p>
+      <p>
+        Type:
         {typeImages.map((typeImg, index) => (
-          <img key={index} src={typeImg} alt={`Type ${pokemon.type[index]}`} 
-          style={{width:200 + "px"}}/>
+          <img
+            key={index}
+            src={typeImg}
+            alt={`Type ${pokemon.type[index]}`}
+            style={{ width: 175 + "px" }}
+          />
         ))}
       </p>
       <p>
         Weaknesses:
         {weaknessImages.map((weaknessImg, index) => (
-          <img key={index} src={weaknessImg} alt={`Weakness ${pokemon.weaknesses[index]}`} 
-          style={{width: 200 + "px"}}/>
+          <img
+            key={index}
+            src={weaknessImg}
+            alt={`Weakness ${pokemon.weaknesses[index]}`}
+            style={{ width: 175 + "px" }}
+          />
         ))}
       </p>
     </div>
